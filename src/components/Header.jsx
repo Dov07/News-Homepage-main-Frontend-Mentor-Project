7847c249fcb736e4a2519703c1717fff53613526
dov07 <olcayvural7@gmail.com>
2026-04-16 23:49:32 +0300

    Mobile responsive header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import logo from '../assets/images/logo.svg'
-const Header = () => {
+import menuIcon from '../assets/images/icon-menu.svg'
+import closeIcon from '../assets/images/icon-menu-close.svg'
 
-  const navLinks = ["Home", "New" , "Popular" ,"Trending" ,"Categories"]
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navLinks = ["Home", "New", "Popular", "Trending", "Categories"];
+
   return (
-    <div className='header'>
-        <a href="#"><img src={logo} alt="logo" /></a>
-       <nav >
+    <header className='header'>
+      <a href="#"><img src={logo} alt="logo" /></a>
+      
+      {/* Hamburger Butonu: Sadece mobilde CSS ile görünecek */}
+      <button className="menu-toggle" onClick={() => setIsMenuOpen(true)}>
+        <img src={menuIcon} alt="open menu" />
+      </button>
+
+      <nav className={`nav-container ${isMenuOpen ? 'open' : ''}`}>
+        {/* Karartma: Sadece mobilde görünür */}
+        <div className="overlay" onClick={() => setIsMenuOpen(false)}></div>
+        
         <ul className='nav-list'>
-        {navLinks.map((e)=>(
-          <li  key={e} className='nav-item' >
-            <a href="#">{e}</a>
+          {/* Mobil Kapatma Butonu: Sadece menü içindeyken mobilde görünür */}
+          <li className="mobile-close-btn">
+             <button onClick={() => setIsMenuOpen(false)}>
+                <img src={closeIcon} alt="close menu" />
+             </button>
           </li>
-        ))}
+
+          {navLinks.map((e)=>(
+            <li key={e} className='nav-item'>
+              <a href="#">{e}</a>
+            </li>
+          ))}
         </ul>
-       </nav>
-    </div>
+      </nav>
+    </header>
   )
 }
 
